23ab11c336e1b571de973a4106fc02eba51ee5f0
Nguyễn Bá Nhật Hoàng <hoangmamg1234@gmail.com>
2026-01-03 19:48:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -37,10 +37,10 @@ task_map = {
 }
 
 # Change variable here
-ALGORITHM_NAME = 'reevo'  # Could also be 'MEoH' or 'NSGA2'
-PROBLEM_NAME = "tsp_semo" # Could also be "tsp_semo, bi_kp, bi_cvrp"
+ALGORITHM_NAME = 'eoh'  # Could also be 'MEoH' or 'NSGA2'
+PROBLEM_NAME = "tri_tsp" # Could also be "tsp_semo, bi_kp, bi_cvrp"
 exact_log_dir_name = "nhv_runtime_50/v1" # must be unique here
-api_key = os.getenv('API_KEY1') # change APIKEY1, APIKEY2, APIKEY3
+API_KEY = os.getenv("MISTRAL_API_KEY")
 
 if __name__ == '__main__':
     
@@ -49,7 +49,7 @@ if __name__ == '__main__':
     TaskClass = task_map[PROBLEM_NAME]
     
     llm = MistralApi(
-        keys=api_key,
+        keys=API_KEY,
         model='codestral-latest',
         timeout=60
     )
